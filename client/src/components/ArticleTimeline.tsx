987527adfdce35cd83250c5ed2e4341f297cccda
re-fract <sidtdm101@gmail.com>
2026-06-21 23:32:37 +0530

    veryyyyyy small change

--- client/src/components/ArticleTimeline.tsx
+++ client/src/components/ArticleTimeline.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import TimelineItem from './TimelineItem'
 import type { TimelineArticle } from '../api/client'
 
--- client/src/components/ParsedArticleBody.tsx
+++ client/src/components/ParsedArticleBody.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 interface ParsedArticleBodyProps {
   text: string
   source?: string
